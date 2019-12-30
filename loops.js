103ab3b7b1c0fe9ae19ca8565edb299a4ae63f17
Marcelo Martinez <marcelomst1@gmail.com>
2019-12-30 21:25:07 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,31 @@
+function forLoop(array){
+  var strangeArray = [...array, new Array(25)]
+  for (let i = 0; i < array.length+25; i++){
+    if (i === 1 || i === 11){
+      strangeArray[i] = `I am 1 strange loop.`  
+    }
+    else if (i === 34){
+      strangeArray[34] = `I am 24 strange loops.`
+    } else { 
+      strangeArray[i] = `"I am ${i} strange loops."` 
+    }
+  }
+  return strangeArray;
+}
+function whileLoop(n){
+  var contdown = n
+  while (contdown>0){
+    console.log(--contdown) 
+  }
+  return 'done'
+}
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+function doWhileLoop(num){
+  var i = 0;
+  do {
+    console.log(`doo-bee-doo-bee-doo=${num}`);
+  } while (incrementVariable() < num);
+}
